f6ae9be66fbe5a4ebd32fc4cb51e3b20de40aa2e
Lucian Cooper <cooperlucian@gmail.com>
2021-03-02 00:53:42 -0500

    feat(core): augment airbnb's `import/no-extraneous-dependencies` rule

--- packages/eslint-config/rules/import.js
+++ packages/eslint-config/rules/import.js
@@ -1,5 +1,36 @@
+const { rules: baseRules } = require('eslint-config-airbnb-base/rules/imports');
+
 module.exports = {
     rules: {
         'import/prefer-default-export': 0,
+        'import/no-extraneous-dependencies': (() => {
+            const [level, { devDependencies, ...options }] = baseRules['import/no-extraneous-dependencies'];
+            return [level, {
+                ...options,
+                devDependencies: [
+                    // jest setup file
+                    '**/setupTests.{js,jsx}',
+                    // for non top-level test directories
+                    '**/{test,tests}/**',
+                    // eslint config
+                    '**/.eslintrc.js',
+                    // stylelint config
+                    '**/.stylelintrc.js',
+                    '**/stylelint.config.js',
+                    // babel config
+                    '**/.babelrc.js',
+                    '**/babel.config.js',
+                    // postcss config
+                    '**/.postcssrc.js',
+                    '**/postcss.config.js',
+                    // top-level tasks
+                    'tasks/**',
+                    // package level tasks in a monorepo
+                    'packages/*/tasks/**',
+                    // globs from base config
+                    ...devDependencies,
+                ],
+            }];
+        })(),
     },
 };
