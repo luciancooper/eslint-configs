148018128f80b0f3be7fe7ffe9723f97f259bfc9
Lucian Cooper <cooperlucian@gmail.com>
2021-03-02 12:38:49 -0500

    test(jest): add file path match tests

---
+++ packages/eslint-config-jest/__tests__/index.js
@@ -0,0 +1,28 @@
+const { ESLint } = require('eslint'),
+    config = require('..');
+
+const linter = new ESLint({
+    useEslintrc: false,
+    allowInlineConfig: false,
+    baseConfig: config,
+});
+
+describe('jest config', () => {
+    test('is applied to files with test suffixes', async () => {
+        await expect(linter.calculateConfigForFile('index.test.js')).resolves.toMatchObject({
+            plugins: ['jest'],
+        });
+        await expect(linter.calculateConfigForFile('index.js')).resolves.not.toMatchObject({
+            plugins: ['jest'],
+        });
+    });
+
+    test('is applied to top-level files in test directories', async () => {
+        await expect(linter.calculateConfigForFile('test/index.js')).resolves.toMatchObject({
+            plugins: ['jest'],
+        });
+        await expect(linter.calculateConfigForFile('test/utils/index.js')).resolves.not.toMatchObject({
+            plugins: ['jest'],
+        });
+    });
+});
