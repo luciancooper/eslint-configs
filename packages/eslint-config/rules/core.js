953c50d07dbd9739b971cd9fcecdf235af33edc1
Lucian Cooper <cooperlucian@gmail.com>
2021-06-08 22:16:10 -0400

    feat(core): require eslint `v7.3` and enable some newer rules

--- packages/eslint-config/rules/core.js
+++ packages/eslint-config/rules/core.js
@@ -10,8 +10,16 @@ module.exports = {
         'consistent-return': [2, {
             treatUndefinedAsUnspecified: true,
         }],
+        // enforce default clauses in switch statements to be last (v7.0.0)
+        'default-case-last': 2,
+        // enforce default parameters to be last (v6.4.0)
+        'default-param-last': 2,
+        // require grouped accessor pairs in object literals and classes (v6.7.0)
+        'grouped-accessor-pairs': [2, 'getBeforeSet'],
         // enforce a maximum number of classes per file
         'max-classes-per-file': 0,
+        // disallow returning value from constructor (v6.7.0)
+        'no-constructor-return': 2,
         // disallow function declarations that contain unsafe references inside loop statements
         'no-loop-func': 0,
         // disallow reassigning `function` parameters
@@ -20,6 +28,8 @@ module.exports = {
         }],
         // disallow `void` operators
         'no-void': 0,
+        // disallow use of the `RegExp` constructor in favor of regular expression literals (v6.4.0)
+        'prefer-regex-literals': 2,
 
         // Possible Errors
 
@@ -27,12 +37,26 @@ module.exports = {
         'no-await-in-loop': 0,
         // disallow the use of `console`
         'no-console': 0,
+        // disallow duplicate conditions in if-else-if chains (v6.7.0)
+        'no-dupe-else-if': 2,
         // disallow empty block statements
         'no-empty': [2, {
             allowEmptyCatch: true,
         }],
+        // disallow assigning to imported bindings (v6.4.0)
+        'no-import-assign': 2,
+        // disallow literal numbers that lose precision (v7.1.0)
+        'no-loss-of-precision': 2,
+        // disallow returning values from Promise executor functions (v7.3.0)
+        'no-promise-executor-return': 2,
+        // disallow returning values from setters (v6.7.0)
+        'no-setter-return': 2,
         // disallow sparse arrays
         'no-sparse-arrays': 0,
+        // disallow loops with a body that allows only one iteration (7.3.0)
+        'no-unreachable-loop': 2,
+        // disallow useless backreferences in regular expressions (v7.0.0)
+        'no-useless-backreference': 2,
 
         // ECMAScript 6
 
@@ -51,10 +75,14 @@ module.exports = {
 
         // Stylistic Issues
 
+        // enforce linebreaks after opening and before closing array brackets
+        'array-bracket-newline': [2, 'consistent'],
         // enforce camelcase naming convention
         camelcase: 0,
         // require or disallow newline at the end of files
         'eol-last': [2, 'never'],
+        // enforce line breaks between arguments of a function call (v6.2.0)
+        'function-call-argument-newline': [2, 'consistent'],
         // enforce consistent indentation
         indent: [2, 4, {
             SwitchCase: 1,
@@ -101,6 +129,8 @@ module.exports = {
         'no-underscore-dangle': 0,
         // enforce variables to be declared either together or separately in functions
         'one-var': [2, 'consecutive'],
+        // disallow the use of `Math.pow` in favor of the `**` operator
+        'prefer-exponentiation-operator': 2, // v6.7.0
 
         // Variables
 
