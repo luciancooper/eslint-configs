8e48073efa9ba90f2edf708dc368432572203190
Lucian Cooper <cooperlucian@gmail.com>
2021-06-16 13:22:53 -0400

    feat(jest): update `overrides` glob patterns

--- packages/eslint-config-jest/index.js
+++ packages/eslint-config-jest/index.js
@@ -3,10 +3,14 @@ module.exports = {
         files: [
             '**/setupTests.[jt]s?(x)',
             '**/jest.setup.[jt]s?(x)',
-            '**/test?(s)/*.[jt]s?(x)',
-            '**/__tests__/*.[jt]s?(x)',
-            '**/__mocks__/*.[jt]s?(x)',
+            '**/test?(s)/**/*.[jt]s?(x)',
+            '**/__@(tests|mocks)__/**/*.[jt]s?(x)',
             '**/+(*.)@(spec|test).[jt]s?(x)',
+            '**/test.[jt]s?(x)',
+        ],
+        excludedFiles: [
+            '**/fixtures/**',
+            '**/__fixtures__/**',
         ],
         extends: [
             require.resolve('./config'),
--- packages/eslint-config-jest/index.test.js
+++ packages/eslint-config-jest/index.test.js
@@ -8,27 +8,52 @@ const linter = new ESLint({
 });
 
 describe('config override globs', () => {
-    test('match files with `.test` and `.spec` suffixes', async () => {
-        await expect(linter.calculateConfigForFile('index.test.js'))
-            .resolves.toIncludePlugin('jest');
-        await expect(linter.calculateConfigForFile('index.spec.js'))
-            .resolves.toIncludePlugin('jest');
-        await expect(linter.calculateConfigForFile('index.js'))
-            .resolves.not.toIncludePlugin('jest');
+    test('match test setup files', async () => {
+        await expect(linter.calculateConfigForFile('setupTests.js')).resolves.toIncludePlugin('jest');
+        await expect(linter.calculateConfigForFile('jest.setup.js')).resolves.toIncludePlugin('jest');
+        await expect(linter.calculateConfigForFile('jest.config.js')).resolves.not.toIncludePlugin('jest');
     });
 
-    test('match top-level files in test directories', async () => {
-        await expect(linter.calculateConfigForFile('test/index.js'))
-            .resolves.toIncludePlugin('jest');
-        await expect(linter.calculateConfigForFile('test/utils/index.js'))
-            .resolves.not.toIncludePlugin('jest');
+    test('match files named `test.*`', async () => {
+        await expect(linter.calculateConfigForFile('test.js')).resolves.toIncludePlugin('jest');
+        await expect(linter.calculateConfigForFile('test.ts')).resolves.toIncludePlugin('jest');
     });
 
-    test('match top-level files in the __mocks__ directory', async () => {
-        await expect(linter.calculateConfigForFile('__mocks__/index.js'))
-            .resolves.toIncludePlugin('jest');
-        await expect(linter.calculateConfigForFile('__mocks__/utils/index.js'))
-            .resolves.not.toIncludePlugin('jest');
+    test('match files with `.test.*` and `.spec.*` suffixes', async () => {
+        await expect(linter.calculateConfigForFile('index.test.js')).resolves.toIncludePlugin('jest');
+        await expect(linter.calculateConfigForFile('index.spec.js')).resolves.toIncludePlugin('jest');
+        await expect(linter.calculateConfigForFile('index.js')).resolves.not.toIncludePlugin('jest');
+    });
+
+    test('does not match snapshot files with extension `.snap`', async () => {
+        await expect(linter.calculateConfigForFile('file.js.snap')).resolves.not.toIncludePlugin('jest');
+        await expect(linter.calculateConfigForFile('__snapshots__/file.js.snap')).resolves.not.toIncludePlugin('jest');
+    });
+
+    describe.each([
+        'test',
+        'tests',
+        '__tests__',
+        '__mocks__',
+    ])('files in `%s/`', (dir) => {
+        test('matches test files', async () => {
+            await expect(linter.calculateConfigForFile(`${dir}/file.js`)).resolves.toIncludePlugin('jest');
+            await expect(linter.calculateConfigForFile(`${dir}/utils/file.js`)).resolves.toIncludePlugin('jest');
+        });
+
+        test('does not match fixture files', async () => {
+            await expect(linter.calculateConfigForFile(`${dir}/fixtures/file.js`))
+                .resolves.not.toIncludePlugin('jest');
+            await expect(linter.calculateConfigForFile(`${dir}/__fixtures__/file.js`))
+                .resolves.not.toIncludePlugin('jest');
+        });
+
+        test('does not match snapshot files', async () => {
+            await expect(linter.calculateConfigForFile(`${dir}/file.js.snap`))
+                .resolves.not.toIncludePlugin('jest');
+            await expect(linter.calculateConfigForFile(`${dir}/__snapshots__/file.js.snap`))
+                .resolves.not.toIncludePlugin('jest');
+        });
     });
 });
 
